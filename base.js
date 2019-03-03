9d8f02cabcad4902f74f3892b3ae15f9413b42e8
Brandon Colbert <expertdash@gmail.com>
2019-03-03 05:58:45 -0500

    0.0.2 r2
    -Removed html from navigation

--- base.js
+++ base.js
@@ -4,19 +4,19 @@ let body = document.getElementsByTagName("body")[0]
 body.innerHTML = `
 	<div class="menuTitle">Brandon's Projects</div>
 	<div class="navMenu" align="right">
-		<a class="navItem" href="/index.html">Home</a>
+		<a class="navItem" href="/index">Home</a>
 		<div class="navItem dropMenu">Projects
 			<div class="dropItems">
-				<a href="/projects/antipodean-chess.html">Antipodean Chess</a>
-				<a href="/projects/carousel-launcher.html">Carousel Launcher</a>
-				<a href="/projects/dogma-of-space.html">Dogma of Space</a>
-				<a href="/projects/fitr.html">Fitr</a>
-				<a href="/projects/iris.html">Project Iris</a>
-				<a href="/projects/tabs.html">Tabs</a>
+				<a href="/projects/antipodean-chess">Antipodean Chess</a>
+				<a href="/projects/carousel-launcher">Carousel Launcher</a>
+				<a href="/projects/dogma-of-space">Dogma of Space</a>
+				<a href="/projects/fitr">Fitr</a>
+				<a href="/projects/iris">Project Iris</a>
+				<a href="/projects/tabs">Tabs</a>
 			</div>
 		</div>
-		<a class="navItem" href="/about.html">About Me</a>
-		<a class="navItem" href="/contact.html">Contact</a>
+		<a class="navItem" href="/about">About Me</a>
+		<a class="navItem" href="/contact">Contact</a>
 	</div>
 	<br><br><br><br>
 ` + body.innerHTML
