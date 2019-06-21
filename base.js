3eeb6275fc5013428d8e0106d912071bf257c665
Brandon Colbert <expertdash@gmail.com>
2019-06-21 08:11:05 -0400

    0.0.5: Redesign and Blogs
    -Complete redesign
    -Modular pages
    -Improved loading method
    -Addition of blogs

--- base.js
+++ base.js
@@ -1,22 +1,51 @@
-document.getElementsByTagName("head")[0].innerHTML = "<title>Brandon's Projects</title>"
+//Get the region where content is displayed
+var content = document.getElementsByClassName("content")[0]
 
-let body = document.getElementsByTagName("body")[0]
-body.innerHTML = `
-	<div class="menuTitle">Brandon's Projects</div>
-	<div class="navMenu" align="right">
-		<a class="navItem" href="/index">Home</a>
-		<div class="navItem dropMenu">Projects
-			<div class="dropItems">
-				<a href="/projects/antipodean-chess">Antipodean Chess</a>
-				<a href="/projects/carousel-launcher">Carousel Launcher</a>
-				<a href="/projects/dogma-of-space">Dogma of Space</a>
-				<a href="/projects/fitr">Fitr</a>
-				<a href="/projects/iris">Project Iris</a>
-				<a href="/projects/tabs">Tabs</a>
-			</div>
-		</div>
-		<a class="navItem" href="/about">About Me</a>
-		<a class="navItem" href="/contact">Contact</a>
-	</div>
-	<br><br><br><br>
-` + body.innerHTML
+//Create a region where different pages content may be displayed
+var subcontent = document.createElement("div")
+subcontent.classList.add("subcontent")
+content.prepend(subcontent)
+
+//Move anything in content to subcontent
+for(let i = 1; i < content.children.length; i++) {
+	subcontent.appendChild(content.children[i])
+	i--
+}
+
+//Add the navbar
+var navbar = document.createElement("div")
+navbar.classList.add("navbar")
+setElementHTML(navbar, "/nav.html")
+content.prepend(navbar)
+
+window.onpopstate = function(event) {
+	setElementHTML(subcontent, window.location.pathname)
+}
+
+/**
+ * Sets the elements innerHTML with content loaded from file location
+ * @param DOM Element to place content in
+ * @param Location of the file containing the content
+ * @return The promise from fetching
+ */
+function setElementHTML(element, location) {
+	return fetch(location).then(content => content.text()).then(text => element.innerHTML = text)
+}
+
+/**
+ * Navigates to and loads the page without reloading the website
+ * @param Location of the file with the content
+ */
+function toPage(location) {
+	history.pushState(null, null, location)
+	setElementHTML(subcontent, location).then(() => {
+		var scc = subcontent.children[0];
+
+		for(let i = 0; i < scc.children.length; i++) {
+			subcontent.appendChild(scc.children[i])
+			i--
+		}
+
+		scc.remove()
+	})
+}
